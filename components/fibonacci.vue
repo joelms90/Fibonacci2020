e2b49dc2e4d14194dbc1714445e0ea90de38ecad
Joel Edmundo Morales Sánchez <joel.morales.unam@gmail.com>
2020-06-15 12:00:00 -0500

    Initial Layout

---
+++ components/fibonacci.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		  <b-form-input v-model="number" placeholder="Ingresa un número entero"></b-form-input>
+		  <div class="links">
+        <button
+          
+          class="button--blue"
+        >
+         
+          Calcular
+        </button>
+      
+      </div>
+		  
+	</div>
+</template>
+<script>
+	export default{
+		data() {
+	      return {
+	        number: null
+	      }
+	    },
+	    methods:{
+
+	    }
+
+	}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,27 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        (Fibonacci2020)
-      </h1>
-      <h2 class="subtitle">
-        My astounding Nuxt.js project
+      <h2 class="title">
+        FIBONACCI
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <b-img src="/logo.png" fluid alt="Logo"></b-img>
+
+      <h3 class="subtitle mt-4">
+        Obten el número inmediato de la serie de Fibonacci
+      </h3>
+      <fibo/>
+      
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
+import fibo from '@/components/fibonacci.vue'
 export default {
-  components: {
-    Logo
+  components:{
+    fibo
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
